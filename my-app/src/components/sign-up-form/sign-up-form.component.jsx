b09b3ac0b97801f43a5c5116ca9556c04e32beca
Donatelo <lazar.kop@hotmail.com>
2022-12-25 00:20:58 +0100

    changes

--- my-app/src/components/sign-up-form/sign-up-form.component.jsx
+++ my-app/src/components/sign-up-form/sign-up-form.component.jsx
@@ -8,8 +8,8 @@ const defaultFormFields = {
 }
 
 const SignUpForm = () => {
-    const { formFields, setFormFields} = useState(defaultFormFields);
-    const { displayName, email, password, confirmPassword } = defaultFormFields;
+    const [ formFields, setFormFields ] = useState(defaultFormFields);
+    const { displayName, email, password, confirmPassword } = formFields;
 
     console.log(formFields);
 
@@ -23,16 +23,16 @@ const SignUpForm = () => {
             <h1>sign up with your email and password</h1>
             <form onSubmit={() => {}}>
                 <label>Display Name</label>
-                <input type='text' required onChange={handleChange} name='displayName' value={displayName}/>
+                <input type='text' required onChange={handleChange} name='displayName' /* value={displayName} *//>
 
                 <label>Email</label>
-                <input type='email' required onChange={handleChange} name='email' value={email}/>
+                <input type='email' required onChange={handleChange} name='email' /* value={email} *//>
 
                 <label>Password</label>
-                <input type='password' required onChange={handleChange} name='password' value={password}/>
+                <input type='password' required onChange={handleChange} name='password' /* value={password} *//>
 
                 <label>Confirm Password</label>
-                <input type='password' required onChange={handleChange} name='confirmPassword' value={confirmPassword}/>
+                <input type='password' required onChange={handleChange} name='confirmPassword' /* value={confirmPassword} *//>
 
                 <button type='submit'>Sign Up</button>
             </form>
