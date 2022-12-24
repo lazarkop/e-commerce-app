ab39efe934d1c36c661420f23e104bde6771eed5
Donatelo <lazar.kop@hotmail.com>
2022-12-24 23:36:35 +0100

    working on sign up form

--- my-app/src/routes/sign-in/sign-in.component.jsx
+++ my-app/src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,21 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { 
+    signInWithGooglePopup,
+    createUserDocumentFromAuth 
+    } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
 
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
-        const userDocRef = createUserDocumentFromAuth(user);
+        const userDocRef = await createUserDocumentFromAuth(user);
     }
-
+    
     return (
       <div>
         <h1>sign in page</h1>
         <button onClick={logGoogleUser} >sign in with google popup</button>
+        <SignUpForm />
       </div>
     );
 };
--- my-app/src/utils/firebase/firebase.utils.js
+++ my-app/src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, SignInWithPopup, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, SignInWithPopup, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -14,13 +14,13 @@ const firebaseConfig = {
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
+  const googleProvider = new GoogleAuthProvider();
+  googleProvider.setCustomParameters({
     prompt: "select_account"
   });
 
   export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+  export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
   export const db = getFirestore(); 
 
   export const createUserDocumentFromAuth = async (userAuth) => {
