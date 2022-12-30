8a474883ed0a0bc7b7010620d16b0af4db01a4bc
Donatelo <lazar.kop@hotmail.com>
2022-12-30 21:53:32 +0100

    changes

--- my-app/src/routes/navigation/navigation.component.jsx
+++ my-app/src/routes/navigation/navigation.component.jsx
@@ -1,27 +1,46 @@
-import { Outlet , Link } from 'react-router-dom';
-import { Fragment} from 'react';
-import { ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+import { Fragment, useContext } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import { UserContext } from '../../contexts/user.context';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
 import './navigation.styles.scss';
 
 const Navigation = () => {
-    return (
-      <Fragment>
-        <div className='navigation'>
-          <Link className='logo-container' to='/'>
-            <CrwnLogo className='logo'/> 
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+
+  const signOutHandler = async () => {
+    await signOutUser();
+    setCurrentUser(null);
+  };
+
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
           </Link>
-          <div className='nav-links-container'>
-            <Link className='nav-link' to='/shop'>
-              SHOP
-            </Link>
+
+          {currentUser ? (
+            <span className='nav-link' onClick={signOutHandler}>
+              SIGN OUT
+            </span>
+          ) : (
             <Link className='nav-link' to='/auth'>
               SIGN IN
             </Link>
-          </div>
+          )}
         </div>
-        <Outlet />
-      </Fragment>
-    )
-  }
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
 
-  export default Navigation;
+export default Navigation;
--- my-app/src/utils/firebase/firebase.utils.js
+++ my-app/src/utils/firebase/firebase.utils.js
@@ -1,74 +1,83 @@
 import { initializeApp } from 'firebase/app';
-import { 
-    getAuth, 
-    GoogleAuthProvider, 
-    signInWithPopup, 
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword 
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCfYRg5xDSO0DDBxeBMB275zzYwMrT_ARQ",
-    authDomain: "clothing-db-dda9e.firebaseapp.com",
-    projectId: "clothing-db-dda9e",
-    storageBucket: "clothing-db-dda9e.appspot.com",
-    messagingSenderId: "64606570493",
-    appId: "1:64606570493:web:8fd0e43d5c0af1eb443b94"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCfYRg5xDSO0DDBxeBMB275zzYwMrT_ARQ",
+  authDomain: "clothing-db-dda9e.firebaseapp.com",
+  projectId: "clothing-db-dda9e",
+  storageBucket: "clothing-db-dda9e.appspot.com",
+  messagingSenderId: "64606570493",
+  appId: "1:64606570493:web:8fd0e43d5c0af1eb443b94"
+};
 
-  const googleProvider = new GoogleAuthProvider();
-  googleProvider.setCustomParameters({
-    prompt: "select_account"
-  });
+const firebaseApp = initializeApp(firebaseConfig);
 
-  export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
-  export const db = getFirestore(); 
+const googleProvider = new GoogleAuthProvider();
 
-  export const createUserDocumentFromAuth = async (
-    userAuth,
-    additionalInformation = {}
-    ) => {
+googleProvider.setCustomParameters({
+  prompt: 'select_account',
+});
 
-    if(!userAuth) return;
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
-    const userDocRef = doc(db, 'users', userAuth.uid);
-    
-    const userSnapshot = await getDoc(userDocRef);
-    
-    // if user does not exist
-    if(!userSnapshot.exists()) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
+export const db = getFirestore();
 
-        try {
-            await setDoc(userDocRef, {
-                displayName,
-                email,
-                createdAt,
-                ...additionalInformation
-            });
-        } catch (error) {
-            console.log('error creating the user', error.message);
-        }
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
 
-        //if user does exist
-        return userDocRef;
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
     }
+  }
+
+  return userDocRef;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
-    if(!email || !password) return;
+  if (!email || !password) return;
 
-    return await createUserWithEmailAndPassword(auth, email, password);
-}
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
 
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-    if(!email || !password) return;
-    
-    return await signInWithEmailAndPassword(auth, email, password);
-}
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
